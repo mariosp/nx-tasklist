a2a56fff24cae00aae306c28769f2c9dc6da9f60
Marios Patsis <mariospatsis@gmail.com>
2023-02-01 02:25:09 +0200

    unit tests

--- client/src/app/ticket-details/ticket-details.spec.tsx
+++ client/src/app/ticket-details/ticket-details.spec.tsx
@@ -1,4 +1,5 @@
-import { render } from '@testing-library/react';
+import { Ticket } from '@acme/shared-models';
+import { act, render } from '@testing-library/react';
 
 import TicketDetails from './ticket-details';
 
@@ -6,5 +7,11 @@ describe('TicketDetails', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<TicketDetails loading={true} tickets={[]} users={[]} handleChange={()=>{}}/>);
     expect(baseElement).toBeTruthy();
+  });
+
+  it('should render no tickets found when is not loading and ticket is not found',  () => {
+    const { baseElement } = render(<TicketDetails loading={false} tickets={[]} users={[]} handleChange={()=>{}}/>);
+
+    expect(baseElement.innerHTML).toContain("No ticket found");
   });
 });
